8bc53759a93f5c12e0d8de00ae3e9352a0204695
AbdulMoiz-Ali <abdulmoizali321@gmail.com>
2024-06-25 21:51:22 +0500

    varaables

---
+++ app.js
@@ -0,0 +1,12 @@
+var username = "my name"
+var message = 'hello would'
+alert('jhone Doe')
+alert('15 year old')
+alert('certified mobile application devalopment')
+alert('PLZZA\rPLZZ\rPLZ\rPL\rP')
+var email = 'My email address is example@example.com'
+alert(email)
+var book = 'A smarter\rway to learn javascript'
+alert('I am trying to learn from the book ' + book)
+var text = 'Yah! I can write HTML conrtent throught javascript'
+document.write(text)
